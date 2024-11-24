ac628d777ed5fa1e7132452fe3983be52b3f16d8
Yaro Shm <yshmarov@gmail.com>
2024-11-24 14:33:07 +0100

    avo profile & cover photos

--- app/avo/resources/listing.rb
+++ app/avo/resources/listing.rb
@@ -8,8 +8,13 @@ class Avo::Resources::Listing < Avo::BaseResource
 
   def fields
     field :id, as: :id
+    field :logo, as: :external_image do
+      record.payload&.dig("favicon_url")
+    rescue
+      nil
+    end
     field :url, as: :text
-    field :payload, as: :text
+    field :payload, as: :text, hide_on: [:index]
     field :categories, as: :has_many, through: :category_listings,
       attach_scope: lambda {
         query.where.not(id: parent.category_listings.select(:category_id))
@@ -19,4 +24,30 @@ class Avo::Resources::Listing < Avo::BaseResource
   def actions
     action Avo::Actions::UrlCrawler
   end
+
+  self.profile_photo = {
+    source: -> {
+      if view.index?
+        # We're on the index page and don't have a record to reference
+        "/icon.png"
+      else
+        # We have a record so we can reference it's profile_photo
+        record.payload&.dig("favicon_url") || "/icon.png"
+      end
+    }
+  }
+
+  self.cover_photo = {
+    size: :lg, # :sm, :md, :lg
+    visible_on: [:show, :forms], # can be :show, :index, :edit, or a combination [:show, :index]
+    source: -> {
+      if view.index?
+        # We're on the index page and don't have a record to reference
+        "/icon.png"
+      else
+        # We have a record so we can reference it's cover_photo
+        record.payload&.dig("og_image_url") || "/icon.png"
+      end
+    }
+  }
 end
